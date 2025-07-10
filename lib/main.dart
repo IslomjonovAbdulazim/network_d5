d6d6ef41f97862a29bb6ee15477be69d9a8ae3fd
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-07-10 17:54:37 +0500

    imp

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,44 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+import 'pages/post_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            CupertinoButton(
+              onPressed: () {
+                Get.to(PostPage());
+              },
+              child: Text("Posts"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
